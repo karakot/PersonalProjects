92af3d5dc6e26b41ceda9f0d5bc2519b199cdbf1
James Lapuz <james.w.lapuz@gmail.com>
2015-06-10 14:30:24 +0800

    Update for more examples

--- TCL/commandCreation.tcl
+++ TCL/commandCreation.tcl
@@ -10,3 +10,11 @@ return $sum
 }
 puts [sum 1 2 3 4]
 puts [sum 1 2 3 4 5 6 7 8 9]
+
+#few Clearly defined Arguments
+proc person {name gender args} {
+    puts "name is $name"
+    puts "gender is $gender"
+    puts "$name 's params are $args"
+}
+person James Male isWorking isSitting 
