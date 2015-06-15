e9066383ee876c8859ed2260939d4a282a07b574
James Lapuz <james.w.lapuz@gmail.com>
2015-06-15 13:51:09 +0800

    Refined fileCreation and added directory creation

--- TCL/fileCreation.tcl
+++ TCL/fileCreation.tcl
@@ -1,14 +1,16 @@
-set dummyTxt "DUMMY DUMMY DUMMY \nDUMMY DUMMY DUMMY"
-
-set fileChannel [open dumb.txt w+]
-puts $fileChannel $dummyTxt
-close $fileChannel
-
+#fileCreation
 proc mkFile {fileName} {
-	set fileChannel [open $fileName w+]
-	puts $fileChannel "DUMMYDUMMYDUMMY\nDUMMYDUMMYDUMMY"
-	close $fileChannel
+        set fileChannel [open $fileName w+]
+        puts $fileChannel "DUMMYDUMMYDUMMY\nDUMMYDUMMYDUMMY"
+        close $fileChannel
 }
-for {set i 1} {$i <= 10} {incr i} { 
-	mkFile dummy${i}.txt
+proc mkFolder {folderName} {
+        file mkdir $folderName
+}
+mkFolder dumbFolder
+for {set j 1} {$j <= 10} {incr j} {
+        mkFolder dumbFolder/folder${j}
+        for {set i 1} {$i <= 10} {incr i} {
+                mkFile dumbFolder/folder${j}/dummy${i}.txt
+        }
 }
